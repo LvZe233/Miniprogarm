f71b9f1fcda4b7665c3376a0787424142bd6b464
LvZe233 <1261186667@qq.com>
2020-07-15 19:08:28 +0800

    知识点1

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 App({                           //小程序关闭时会在后台存活5分钟，五分钟内重新打开不会进行初始化，直接onShow
   onLaunch:function(){          //小程序初始化完成执行
    console.log("小程序初始化完成： onluanch")
-    wx.getUserInfo({            //异步调用
-      success:function(res){
-        console.log(res)
-      }
-    })
   },
 
   onShow:function(options){     //小程序界面显示完成
     console.log("界面显示出来： onShow")
     console.log(options.scene)
+    //1.判断小程序的进入场景
     switch(options.scene){
       case 1001:
         break;
