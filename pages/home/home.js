23794689c76126b27b9dec103c6cb3d78f4b74e1
LvZe233 <1261186667@qq.com>
2020-07-28 13:48:40 +0800

    知识点22

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,55 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中',
+      duration:3000,
+      icon:"loading",
+      //image:"/asserts/taBarIcon/button1.png"
+      success:function(){
+        console.log("显示弹窗成功")
+      },
+      fail:function(){
+        console.log("显示弹窗失败")
+      },
+      complete:function(){
+        console.log("完成函数调用")
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      cancelColor: 'red',
+      cancelText:'退出',
+      //showCancel:false,
+      title: '我是标题',
+      content:'我是内容',
+      //点击了确定或者取消就会进入success的回调函数
+      success:function(res){
+        if(res.cancel){
+          console.log("用户点击了取消按钮")
+        }
+        else
+          console.log("用户点击了确定按钮")
+      }
+  })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载ing',
+      mask:true,
+    })
+    setTimeout(()=>{
+      //必须要手动关闭
+      wx.hideLoading()
+    },2000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['照片','拍照'],
+      itemColor:'red',
+      success:function(res){
+        console.log(res)
+      }
+    })
   }
 })
