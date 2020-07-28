35974e55b0feb26d396961d712f61a3bdb1c2365
LvZe233 <1261186667@qq.com>
2020-07-28 23:17:54 +0800

    知识点24

--- app.js
+++ app.js
@@ -1,30 +1,73 @@
-App({                           //小程序关闭时会在后台存活5分钟，五分钟内重新打开不会进行初始化，直接onShow
-  onLaunch:function(){          //小程序初始化完成执行
-   console.log("小程序初始化完成： onluanch")
-    wx.getUserInfo({            //异步调用
-      success:function(res){
-        console.log(res)
+const TOKEN = 'token'
+
+App({     
+  //globalData 对象存在内存之中，小程序关闭之后会消失  
+  globalDate:{
+    token:''
+  } ,            
+
+  onLaunch:function(){         
+    //1.先从缓存中取出token
+    const token = wx.getStorageSync(TOKEN)
+    //2.判断token中是否有值
+    if (token && token.length != 0){    //已经有token，验证token是否过期
+      this.check_token(token)     //验证token是否过期
+    }
+    else { //没有token，进行登陆操作
+      this.login()
+    }
+  },
+
+  check_token(token){
+    console.log('执行了验证token操作')
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method:'POST',
+      header:{
+        token:token
+      },
+      success:(res)=>{
+        if(!res.data.errCode){
+          this.globalDate.token = token
+        }
+        else{
+          this.login()
+        }
+      },
+      fail:function(err){
+        console.log(err)
       }
     })
   },
 
-  onShow:function(options){     //小程序界面显示完成
-    console.log("界面显示出来： onShow")
-    console.log(options.scene)
-    switch(options.scene){
-      case 1001:
-        break;
-      case 1005:
-        break;
-    }
-  },
+  login(){
+    console.log("执行了登陆操作")
+    //登陆操作
+    wx.login({
+      timeout: 30000,
+      //code只有5分钟的有效期
+      success:(res)=>{
+      //获取code
+      const code = res.code;
 
-  onHide:function(){             //界面被隐藏时调用
-    console.log("界面被隐藏： onHide")
-  },
+      //将code发送给服务器
+      wx.request({
+        url: 'http://123.207.32.32:3000/login',
+        method:"POST",
+        data:{
+          code:code
+        },
+        success:(res)=>{
+          //取出token
+          const token = res.data.token;
 
-  onError:function(msg){
-    console.log("小程序发生了错误")
+          //将token保存到 globalData 中
+          this.globalDate.token = token
+          wx.setStorageSync(TOKEN, this.globalDate.token)
+        }
+      })
+      }
+    })
   }
 
 })
